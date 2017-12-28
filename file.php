14556edb6424f89dd574735c12d43463758ba012
HazaraPixels <34315970+uxeys@users.noreply.github.com>
2017-12-28 01:09:27 +0100

    upload a user test

---
+++ file.php
@@ -0,0 +1,13 @@
+<?php
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "Mickey Mouse\n";
+fwrite($myfile, $txt);
+$txt = "Minnie Mouse\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
